4848a49205b3d69d3f41a531306f92af0dbdd853
unknown <jore5523@gmail.com>
2020-05-10 16:27:07 -0400

    initial author routes

--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
+const mongoose = require("mongoose");
 
+//Setting the routes
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
+//middlewares
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 
-const mongoose = require("mongoose");
 //DB config
 const dbPath = process.env.DATABASE_URL;
 //global flags sets for eliminate mongo flags
@@ -32,6 +37,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", (error) => console.log("Connected to Moongose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App listening on port 3000!");
